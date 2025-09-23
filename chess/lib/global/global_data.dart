930d78611c7413210956126d97461fe93d761f0f
123xiaobinggan <1398865656@qq.com>
2025-09-23 21:58:43 +0800

    update to v1.1.0

--- chess/lib/global/global_data.dart
+++ chess/lib/global/global_data.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 
 class GlobalData extends GetxController {
-  static var url = "http://120.48.156.237:3000";
+  static var url = "{数据库地址}";
   static var socketService;
   static var userInfo = <String, dynamic>{
     'accountId': '',
--- server/db.js
+++ server/db.js
@@ -1,14 +1,14 @@
 // db.js
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://chessUser:Qinguanqiao1356@120.48.156.237:27017/ChessKing'; // 改成你的数据库地址
+const url = 'mongodb://{数据库名称}:{服务器密码}{服务器地址+工作文件夹}'; // 改成你的数据库地址
 const client = new MongoClient(url);
 
 async function connectDB() {
   if (!client.topology?.isConnected()) {
     await client.connect();
   }
-  return client.db('ChessKing'); // 改成你的数据库名
+  return client.db('{数据库名}'); // 改成你的数据库名
 }
 
 module.exports = connectDB;
